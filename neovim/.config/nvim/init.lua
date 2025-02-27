e79407299850ef919bea0bb6650228cd546d3e39
Disvster <mmcouto2001@gmail.com>
2025-02-27 04:10:38 +0000

    changed to catpuccin-machiatto on kitty and nvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,8 @@
+-- Lazy PKGM
 require("config.lazy")
+-- colorscheme "catppuccin-macchiato"
 
+-- VIM settings
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.cmd.colorscheme "catppuccin-frappe"
