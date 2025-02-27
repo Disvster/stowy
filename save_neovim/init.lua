7a3edf4513fb395585ffad7bb61e36fd584fc062
Disvster <mmcouto2001@gmail.com>
2025-02-27 18:42:49 +0000

    couldnt add transparency background for tokyonight

--- save_neovim/init.lua
+++ save_neovim/init.lua
@@ -6,14 +6,14 @@ require("config.lazy")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 --vim.cmd.colorscheme "catppuccin-frappe"
-vim.cmd.colorscheme "catppuccin-macchiato"
+vim.cmd.colorscheme("catppuccin-macchiato")
 --vim.cmd.colorscheme "catppuccin-latte"
 --vim.cmd.colorscheme "catppuccin-mocha"
 
 -- LineNumberColors
-    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='gray', bold=false })
-    vim.api.nvim_set_hl(0, 'LineNr', { fg='#ffe5b4', bold=true })
-    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='gray', bold=false })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "gray", bold = false })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffe5b4", bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "gray", bold = false })
 
 -- Icons
 --use 'yamatsum/nvim-nonicons'
