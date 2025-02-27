bdd450e256f9f4d5e70e015a99faa7ad2232a517
Disvster <mmcouto2001@gmail.com>
2025-02-27 05:26:56 +0000

    applied theme and transparent background on neovim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -5,4 +5,13 @@ require("config.lazy")
 -- VIM settings
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.cmd.colorscheme "catppuccin-frappe"
+--vim.cmd.colorscheme "catppuccin-frappe"
+vim.cmd.colorscheme "catppuccin-macchiato"
+--vim.cmd.colorscheme "catppuccin-latte"
+--vim.cmd.colorscheme "catppuccin-mocha"
+
+-- LineNumberColors
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='gray', bold=false })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='#ffe5b4', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='gray', bold=false })
+
