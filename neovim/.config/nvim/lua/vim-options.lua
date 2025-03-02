bf05f7ad9a3bd328b08e8242c2be8e58bb2ba01f
Disvster <mmcouto2001@gmail.com>
2025-03-02 15:02:17 +0000

    added init back up file, will now change init.lua plugins

--- neovim/.config/nvim/lua/vim-options.lua
+++ neovim/.config/nvim/lua/vim-options.lua
@@ -1,8 +1,8 @@
 vim.cmd.colorscheme("tokyonight-moon")
 
 -- numbers color set to gray
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "gray", bold = false })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "gray", bold = false })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "orange", bold = false })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "yellow", bold = false })
 -- comment colors
 vim.api.nvim_set_hl(0, "Comment", { fg = "gray" })
 vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
