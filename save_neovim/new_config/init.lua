9b4033aa063a2a95bb4fa9ea959b49ceee8e3ff9
Disvster <mmcouto2001@gmail.com>
2025-04-05 03:50:17 +0100

    finally finished modular plugin neovim conf!

--- save_neovim/new_config/init.lua
+++ save_neovim/new_config/init.lua
@@ -18,11 +18,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
+require("lazy").setup({
 	{ import = "plugins.tokyonight" },
---	{ import = "plugins.catppuccin" },
+	{ import = "plugins.catppuccin" },
 	{ import = "plugins.vim-sleuth" },
-	{ import = "plugins.git-signs" },
+	{ import = "plugins.gitsigns" },
 	{ import = "plugins.which-key" },
 	{ import = "plugins.telescope" },
 	{ import = "plugins.LSP.lazydev" },
@@ -32,6 +32,28 @@ require("lazy").setup(
 	{ import = "plugins.todo-comments" },
 	{ import = "plugins.mini" },
 	{ import = "plugins.nvim-treesitter" },
-	{ import = "plugins.neo-tree" })
+	{ import = "plugins.neo-tree" },
+	{ import = "plugins.health" },
+}, {
+	ui = {
+	-- If you are using a Nerd Font: set icons to an empty table which will use the
+	-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
 
 require("vim-options")
