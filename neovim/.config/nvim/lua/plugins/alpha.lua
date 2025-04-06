ed42cd0e9f79b61a4bfb945f812770c6c90595bc
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-06 21:14:06 +0100

    added 42 kitty

--- neovim/.config/nvim/lua/plugins/alpha.lua
+++ neovim/.config/nvim/lua/plugins/alpha.lua
@@ -39,9 +39,9 @@ return {
     -- Basic dashboard buttons
     dashboard.section.buttons.val = {
       dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
-      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
       dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
-      dashboard.button("q", "  Quit", ":qa<CR>"),
+      dashboard.button("q", "  Quit", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
