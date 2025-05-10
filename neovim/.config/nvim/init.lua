79942fd3756aef801825f74276ba74d30171ea8e
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-05-10 18:18:00 +0100

    changed ascii

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
 	{ import = "plugins.persistence" },
 	{ import = "plugins.themery" },
 	{ import = "plugins.42_header" },
-	{ import = "plugins.copilotChat" },
+--	{ import = "plugins.copilotChat" },
 	{ import = "plugins.42_norminette" }
 }, {
 	ui = {
