e79747ad2cb12808086b5732073c07a51409942e
Disvster <disaster@selene.localdomain>
2025-11-05 18:54:46 +0000

    super colliderkeybinds comments

--- neovim/.config/nvim/lua/plugins/supercollider/scnvim.lua
+++ neovim/.config/nvim/lua/plugins/supercollider/scnvim.lua
@@ -40,3 +40,23 @@ return {
 --     <space>scq - Toggle post window (think "q" for query)
 --     <space>scp - Clear post window
 --     <space>sc. - Hard stop (emergency stop all sound)
+-- other keybinds:
+  -- local map = scnvim.map
+  -- scnvim.setup {
+  --   keymaps = {
+  --     ['<M-e>'] = map('editor.send_line', {'i', 'n'}),
+  --     ['<C-e>'] = {
+  --       map('editor.send_block', {'i', 'n'}),
+  --       map('editor.send_selection', 'x'),
+  --     },
+  --     ['<CR>'] = map('postwin.toggle'),
+  --     ['<M-CR>'] = map('postwin.toggle', 'i'),
+  --     ['<M-L>'] = map('postwin.clear', {'n', 'i'}),
+  --     ['<C-k>'] = map('signature.show', {'n', 'i'}),
+  --     ['<F12>'] = map('sclang.hard_stop', {'n', 'x', 'i'}),
+  --     ['<leader>st'] = map(scnvim.start),
+  --     ['<leader>sk'] = map(scnvim.recompile),
+  --     ['<F1>'] = map_expr('s.boot'),
+  --     ['<F2>'] = map_expr('s.meter'),
+  --   },
+  -- }
