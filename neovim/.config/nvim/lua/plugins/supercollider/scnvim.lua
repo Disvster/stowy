3bd14b8b64da2d0b248d8222ae2e90b5dd1d8dc2
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-11-05 18:52:33 +0000

    changed keybinds for supercollider

--- neovim/.config/nvim/lua/plugins/supercollider/scnvim.lua
+++ neovim/.config/nvim/lua/plugins/supercollider/scnvim.lua
@@ -6,28 +6,28 @@ return {
     local map = scnvim.map
     local map_expr = scnvim.map_expr
     scnvim.setup {
-      keymaps = {
-        ['<localleader>sc'] = {
-          map('editor.send_line', '<CR>'),
-          map('editor.send_block', '<M-CR>'),
-          map('editor.send_selection', '<CR>'),
-        },
-		['<localleader>scb'] = map('sclang.boot'),
-		['<localleader>scr'] = map('sclang.recompile'),
-		['<localleader>sck'] = map('sclang.start'),
-		['<localleader>sct'] = map('sclang.stop'),
-		['<localleader>scq'] = map('postwin.toggle'),
-		['<localleader>scp'] = map('postwin.clear'),
-		['<localleader>sc.'] = map('sclang.hard_stop'),
-      },
-	  extensions = {
-		piano = {
-		default_synth = 'default',  -- or your custom synth name
-		note_dur = 0.5              -- default note duration in seconds
-		}
-	  }
+		keymaps = {
+			['<localleader>sc'] = {
+			  map('editor.send_line', {'i', 'n'}),
+			  map('editor.send_block', {'i', 'n'}),
+			  map('editor.send_selection', 'x'),
+			},
+			['<localleader>scb'] = map('sclang.boot'),
+			['<localleader>scr'] = map('sclang.recompile'),
+			['<localleader>sck'] = map('sclang.start'),
+			['<localleader>sct'] = map('sclang.stop'),
+			['<localleader>scq'] = map('postwin.toggle'),
+			['<localleader>scp'] = map('postwin.clear'),
+			['<localleader>sc.'] = map('sclang.hard_stop'),
+		},
+		extensions = {
+			piano = {
+			default_synth = 'default',  -- or your custom synth name
+			note_dur = 0.5              -- default note duration in seconds
+			},
+		},
 	}
-    scnvim.load_extension('piano')
+	scnvim.load_extension('piano')
   end
 }
 -- The keybindings now are:
